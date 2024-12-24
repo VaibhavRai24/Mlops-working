abd043d6029b653edc38fabac557b7c049c5585e
VaibhavRai24 <vairaibhav922@gmail.com>
2024-12-24 16:45:23 +0530

    Experiment tracking done with the help of dvclive

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,7 +5,8 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging 
-from dvclive import live
+from dvclive import Live
+import yaml
 
 log_dir = 'logs'
 os.makedirs(log_dir, exist_ok=True)
@@ -26,6 +27,23 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def load_model(file_path:str):
     """Load the train model from a file"""
@@ -64,7 +82,7 @@ def evaluate_model(clf, X_test:np.ndarray, y_test:np.ndarray)->dict:
         
         accuracy = accuracy_score(y_test, y_pred)
         precision = precision_score(y_test, y_pred, average='macro')
-        recall = recall_score(y_test, y_pred,average='macro')
+        recall = recall_score(y_test, y_pred, average='macro')
 
         metrics_dict = {
             'accuracy': accuracy,
@@ -92,6 +110,7 @@ def save_metrices(metrices:dict, file_path:str) ->None:
     
 def main():
     try:
+        params = load_params(params_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -99,6 +118,14 @@ def main():
         y_test = test_data.iloc[:, -1].values
 
         metrics = evaluate_model(clf, X_test, y_test)
+        
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test, average='macro'))
+            live.log_metric('recall', recall_score(y_test, y_test, average='macro'))
+
+            live.log_params(params)
+            
         save_metrices(metrics, 'reports/metrics.json')
         
     except Exception as e:
